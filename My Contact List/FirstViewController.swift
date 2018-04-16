99254b7eab0df80b87aa43863cdde8cb0739d95a
Garrett <garrett.voss4@gmail.com>
2018-04-16 11:35:34 -0500

    Initial Commit

---
+++ My Contact List/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  My Contact List
+//
+//  Created by Voss, Garrett on 4/16/18.
+//  Copyright © 2018 Voss, Garrett. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
