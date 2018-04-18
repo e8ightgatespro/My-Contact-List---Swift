abc6f53e3d61d4c730ef8f8835cfed8bd653114a
Garrett <garrett.voss4@gmail.com>
2018-04-17 20:25:46 -0500

    Started up on page 200

--- My Contact List/ContactsViewController.swift
+++ My Contact List/ContactsViewController.swift
@@ -1,4 +1,3 @@
-//
 //  ContactsViewController.swift
 //  My Contact List
 //
@@ -21,7 +20,38 @@ class ContactsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBOutlet weak var scrollView: UIScrollView!
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.registerKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.unregisterKeyboardNotifications()
+    }
+    
+    func registerKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector:
+            #selector(ContactsViewController.keyboardDidShow(notification:)), name:
+            NSNotification.Name.UIKeyboardDidShow, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector:
+            #selector(ContactsViewController.keyboardWillHide(notification:)), name:
+            NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    func unregisterKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    
+    func keyboardDidShow(notification: NSNotification) {
+        let userInfo: NSDictionary = notification.userInfo! as NSDictionary
+        
+    }
+    
+    
     /*
     // MARK: - Navigation
 
