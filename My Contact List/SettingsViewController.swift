b28195f8c54ab0b18f729f76fb4f4cf100fadd7a
Garrett <garrett.voss4@gmail.com>
2018-04-18 19:10:26 -0500

    added picker and witch to settings scene. Added items to the picker and print the selection to the console

--- My Contact List/SettingsViewController.swift
+++ My Contact List/SettingsViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
-
+class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    @IBOutlet weak var pckSortField: UIPickerView!
+    @IBOutlet weak var swAscending: UISwitch!
+    
+    let sortOrderItems: Array<String> = ["ContactName", "City", "Birthday"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        pckSortField.dataSource = self
+        pckSortField.delegate = self
         // Do any additional setup after loading the view.
     }
 
@@ -21,7 +27,26 @@ class SettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func sortDirectionChanged(_ sender: Any) {
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return sortOrderItems.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return sortOrderItems[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("Chosen Item: \(sortOrderItems[row])")
+    }
+    
     /*
     // MARK: - Navigation
 
